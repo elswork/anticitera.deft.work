740c781f8cc795f35cdac6b811d13c35d51d9442
Eloy López <elswork@gmail.com>
2023-11-23 23:00:26 +0100

    añadir metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,6 +3,8 @@ module.exports = {
 	url: "https://anticitera.deft.work",
 	language: "es",
 	description: "En este proyecto se contiene el conocimiento relevante para el asistente Anticitera de OpenAI y la información para promover la creación del dominio de nivel superior .IA",
+	analytics: "G-FRM23DV6JJ",
+	googletag: "GTM-PCMLMBWD",
 	author: {
 		name: "Eloy López Sánchez",
 		email: "elswork@gmail.com",
