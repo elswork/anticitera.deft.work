9b8b011088621c4e513a8644b401e09a86320435
Eloy López <elswork@gmail.com>
2023-12-02 23:01:02 +0100

    crear links.json

--- links.js
+++ links.js
@@ -4,19 +4,6 @@ const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
 
-function emptyDirectory(directory) {
-    // Leer todos los archivos en el directorio
-    fs.readdir(directory, (err, files) => {
-        if (err) throw err;
-
-        for (const file of files) {
-            fs.unlink(path.join(directory, file), err => {
-                if (err) throw err;
-            });
-        }
-    });
-}
-
 async function extractMetadataAndImages(url) {
     const response = await fetch(url);
     const html = await response.text();
@@ -52,33 +39,6 @@ async function extractMetadataAndImages(url) {
     return { title, description, imageUrls: uniqueImageUrls };
 }
 
-async function createMarkdownFile(url, metadata) {
-    let content = `---
-title: "${metadata.title}"
-description: "${metadata.description}"
-url: "${url}"
----
-
-# [${metadata.title}](${url})
-
-${metadata.description}
-
-`;
-
-    // Añadir imágenes al contenido Markdown
-    metadata.imageUrls.forEach(imgUrl => {
-        content += `![Imagen](${imgUrl})\n`;
-    });
-
-    const filename = path.basename(url) + '.md';
-    const linksDir = path.join(__dirname, 'content', 'links');
-    if (!fs.existsSync(linksDir)) {
-        fs.mkdirSync(linksDir, { recursive: true });
-    }
-    const filePath = path.join(linksDir, filename);
-    fs.writeFileSync(filePath, content);
-}
-
 async function processFile(filePath) {
     const fileStream = fs.createReadStream(filePath);
 
@@ -87,21 +47,23 @@ async function processFile(filePath) {
         crlfDelay: Infinity
     });
 
+    const linksData = []; // Arreglo para almacenar los datos de cada link
+
     for await (const line of rl) {
         if (line) {
             try {
                 const metadata = await extractMetadataAndImages(line);
-                createMarkdownFile(line, metadata);
+                // Agregar los metadatos al arreglo
+                linksData.push({ url: line, ...metadata });
             } catch (error) {
                 console.error(`Error processing URL ${line}:`, error);
             }
         }
     }
+
+    // Escribir el arreglo como un archivo JSON
+    fs.writeFileSync(path.join(__dirname, '_data', 'links.json'), JSON.stringify(linksData, null, 2));
 }
 
 const linksFilePath = path.join(__dirname, '_data', 'links');
-const linksDir = path.join(__dirname, 'content', 'links');
-
-// Vaciar la carpeta antes de procesar nuevos archivos
-emptyDirectory(linksDir);
 processFile(linksFilePath);
