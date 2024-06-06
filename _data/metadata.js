4035c0bb4f03729c56226c3c990124518a6f6ab3
elswork <elswork@gmail.com>
2024-06-06 14:48:46 +0200

    Cambio Google tag

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,7 +3,7 @@ module.exports = {
 	url: "https://anticitera.deft.work/",
 	language: "es",
 	description: "En este proyecto se contiene el conocimiento relevante para el asistente Anticitera de OpenAI y la información para promover la creación del dominio de nivel superior .IA",
-	analytics: "G-FRM23DV6JJ",
+	analytics: "G-5G2G9LM5Z4",
 	googletag: "GTM-PCMLMBWD",
 	author: {
 		name: "Eloy López Sánchez",
