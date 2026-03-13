90d02454cc6fba22977324ab1e7671a9162800fc
Eloy López <elswork@gmail.com>
2026-03-13 22:43:45 +0100

    feat(seo): Operación Faro - Metadatos, JSON-LD, Ensayo Soberanía e Imagen

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
-	title: "Anticitera // Nación Digital",
+	title: "Nación Digital Anticitera | Soberanía e Inteligencia Aumentada (.IA)",
 	url: "https://anticitera.deft.work/",
 	language: "es",
-	description: "Anticitera es un estado mental. El punto cero de la computación. Una Nación Digital descentralizada donde la Inteligencia Aumentada (.IA) es nuestra cultura y código.",
+	description: "Descubre Anticitera, la Nación Digital descentralizada. Promovemos la soberanía tecnológica, el dominio soberano .IA y la Alianza Algorítmica (Inteligencia Aumentada).",
 	analytics: "G-FRM23DV6JJ",
 	googletag: "GTM-PCMLMBWD",
 	author: {
